6a85120dd55e76adda9ee51c9977cf2c6795c743
Cree <cree@provinsal.com>
2019-03-21 18:27:48 -0700

    Replace .babelrc with babel.config.js
    Also updated all lesson's package.json files. 
    - Removed Jest 
    - Removed Mocha
    - Removed @babel/register
    - Add Jest

--- lessons/01-manage-application-state-with-immutable-js/src/index.js
+++ lessons/01-manage-application-state-with-immutable-js/src/index.js
@@ -10,12 +10,3 @@ export function updateState(immutable, pos, value) {
 
 
 export default {mutateValue, updateState}
-
-// // An evil rogue function!
-// function mutateValue(iterable, pos, value) {
-//   iterable[pos] = value;
-// }
-
-// function updateState(immutable, pos, value) {
-//   return immutable.set(pos, value);
-// }

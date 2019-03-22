6e16e1ad775ca13ede100aa6f7914dd82c4de45c
Cree <cree@provinsal.com>
2019-03-22 13:14:37 -0700

    Add React and ReactDOM to lesson 12

--- lessons/12-simple-undoredo-with-immutablejs-data-structures/src/index.js
+++ lessons/12-simple-undoredo-with-immutablejs-data-structures/src/index.js
@@ -1,6 +1,6 @@
-import { Map, List } from "immutable";
-import React from "react";
-import { render } from "react-dom";
+import { render } from 'react-dom'
+import React from 'react'
+import { Map, List } from 'Immutable'
 
 class Model {
   constructor() {
@@ -9,53 +9,50 @@ class Model {
   }
 
   undo() {
-    if (this.historyIndex > 0) this.historyIndex--;
-
-    this.update();
+    if (this.historyIndex > 0) 
+      this.historyIndex--;
+    
+     this.update();
   }
-
+  
   redo() {
-    if (this.historyIndex < this.history.size) this.historyIndex++;
-
+    if (this.historyIndex < this.history.size) 
+      this.historyIndex++;
+    
     this.update();
   }
-
+  
   addPoint(point) {
     this.performOperation(data => {
-      return data.push(
-        Map({
-          x: point.x,
-          y: point.y,
-          id: +new Date()
-        })
-      );
+      return data.push(Map({
+        x: point.x,
+        y: point.y,
+        id: +new Date()
+      }))
     });
   }
-
+  
   performOperation(fn) {
     this.history = this.history.slice(0, this.historyIndex + 1);
-    let newVersion = fn(this.history.get(this.historyIndex));
+    let newVersion = fn( this.history.get(this.historyIndex));
     this.history = this.history.push(newVersion);
     this.historyIndex++;
-
+    
     this.update();
   }
-
+  
   update() {
-    renderComponent({ points: this.history, historyIndex: this.historyIndex });
+    renderComponent({points: this.history, historyIndex: this.historyIndex});
   }
 }
 
 var model = new Model();
 
 class DrawingCanvas extends React.Component {
-  handleUndo() {
-    model.undo();
-  }
-  handleRedo() {
-    model.redo();
-  }
-
+  
+  handleUndo() { model.undo(); }
+  handleRedo() { model.redo(); }
+  
   handleArtboardClick(event) {
     let point = {
       x: event.clientX - event.target.getBoundingClientRect().left,
@@ -63,24 +60,17 @@ class DrawingCanvas extends React.Component {
     };
     model.addPoint(point);
   }
-
+  
   render() {
+    
     var redoDisabled = true;
     var undoDisabled = true;
     var circles = [];
-
+    
     if (this.props.points && this.props.historyIndex) {
       this.props.points.get(this.props.historyIndex).forEach(point => {
-        circles.push(
-          <circle
-            key={point.get("id")}
-            r="5"
-            cx={point.get("x")}
-            cy={point.get("y")}
-            fill="#000000"
-          />
-        );
-      });
+          circles.push(<circle key={point.get('id')} r="5" cx={point.get('x')} cy={point.get('y')} fill="white" />);
+        });
 
       undoDisabled = this.props.historyIndex === 0;
       redoDisabled = this.props.historyIndex === this.props.points.size - 1;
@@ -88,29 +78,28 @@ class DrawingCanvas extends React.Component {
 
     return (
       <div className="drawing-canvas container">
-        <svg
-          ref="artboard"
-          className="artboard container"
-          onClick={this.handleArtboardClick.bind(this)}
-        >
+      
+        <svg ref="artboard" 
+             className="artboard container" 
+             onClick={this.handleArtboardClick.bind(this)}>
           {circles}
         </svg>
-
+      
         <div className="button-holder">
-          <button onClick={this.handleUndo.bind(this)} disabled={undoDisabled}>
-            Undo
-          </button>
-          <button onClick={this.handleRedo.bind(this)} disabled={redoDisabled}>
-            Redo
-          </button>
+          <button onClick={this.handleUndo.bind(this)}
+            disabled={undoDisabled}>Undo</button>
+          <button onClick={this.handleRedo.bind(this)}
+            disabled={redoDisabled}>Redo</button>
         </div>
+      
       </div>
     );
   }
-}
+  
+} 
 
 function renderComponent(props) {
-  render(<DrawingCanvas {...props} />, document.getElementById("app"));
+  render(<DrawingCanvas {...props} />, document.getElementById('app'));
 }
 
 renderComponent();
